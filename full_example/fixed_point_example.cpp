8537eb0777ef410e0c777db63948467072f668d2
KozakNazar <55637421+KozakNazar@users.noreply.github.com>
2023-04-19 17:53:26 +0300

    Add files via upload

--- full_example/fixed_point_example.cpp
+++ full_example/fixed_point_example.cpp
@@ -1,31 +1,30 @@
 #include "stdio.h"
 #include "math.h"
 
-#define DATA_TYPE int
-
-DATA_TYPE data[] = { -1, -2, -5, -4, -4 };
+int data[] = { -1, -2, -5, -4, -4 }, result;
 
 #define DATA data
 #define DATA_LEN (sizeof( DATA ) / sizeof(0[ DATA ]))
 #define BYTE_COUNT_AFTER_POINT (sizeof(0[ DATA ]) / 2)
 #define BIT_COUNT_AFTER_POINT (8 * BYTE_COUNT_AFTER_POINT)
-#define POINT_CONTINUE ((DATA_TYPE)ceil(pow(2, (double)BIT_COUNT_AFTER_POINT)))
+#define POINT_CONTINUE ((long long int)ceil(pow(2, (double)BIT_COUNT_AFTER_POINT)))
 #define AFTER_POINT_MASK ((long long int)POINT_CONTINUE - 1)
-#define DECIMAL_DIGIT_COUNT_AFTER_POINT ((DATA_TYPE)ceil(log10((double)AFTER_POINT_MASK)))
-#define DECIMAL_DIGIT_SHIFT_AFTER_POINT ((DATA_TYPE)floor(pow(10, (double)DECIMAL_DIGIT_COUNT_AFTER_POINT)))
+#define DECIMAL_DIGIT_COUNT_AFTER_POINT ((long long int)ceil(log10((double)AFTER_POINT_MASK)))
+#define DECIMAL_DIGIT_SHIFT_AFTER_POINT ((long long int)floor(pow(10, (double)DECIMAL_DIGIT_COUNT_AFTER_POINT)))
 
 int main() {
-	const DATA_TYPE point_continue = POINT_CONTINUE;
-	const DATA_TYPE after_point_mask = AFTER_POINT_MASK;
-	const DATA_TYPE decimal_digit_shift_after_point = DECIMAL_DIGIT_SHIFT_AFTER_POINT / 10 + 1;
+	const long long int bit_count_after_point = BIT_COUNT_AFTER_POINT;
+	//const long long int point_continue = POINT_CONTINUE;
+	const long long int after_point_mask = AFTER_POINT_MASK;
+	const long long int decimal_digit_shift_after_point = DECIMAL_DIGIT_SHIFT_AFTER_POINT / 10 + 1;
 
 	// I - set point
 	for (unsigned int index = 0; index < DATA_LEN; ++index) {
-		data[index] <<= BIT_COUNT_AFTER_POINT;
+		data[index] <<= bit_count_after_point;
 	}
 
 	// II - compute
-	DATA_TYPE result = 0;
+	//DATA_TYPE result = 0;
 	char signStr[2] = { '\0', '\0' };
 	for (unsigned int index = 0; index < DATA_LEN; ++index) {
 		result += data[index];
@@ -37,7 +36,8 @@ int main() {
 	result /= DATA_LEN;
 
 	// III - output
-	printf("%s%lld,%lld\r\n", signStr, (long long int)result / point_continue, (long long int)(result & after_point_mask) * decimal_digit_shift_after_point / point_continue);
+	printf("%s%lld,%lld\r\n", signStr, (long long int)(result >> bit_count_after_point), (long long int)((result & after_point_mask) * decimal_digit_shift_after_point >> bit_count_after_point));
+	//printf("%s%lld,%lld\r\n", signStr, (long long int)result / point_continue, (long long int)(result & after_point_mask) * decimal_digit_shift_after_point / point_continue);
 
 	return 0;
 }
